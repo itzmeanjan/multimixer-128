aca4dff6094cd0211ec7517c4dd6c53e30bda3dd
Anjan Roy <hello@itzmeanjan.in>
2023-10-07 16:08:13 +0530

    implement universal keyed hash function multimixer-128
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benches/multimixer_128.rs
+++ benches/multimixer_128.rs
@@ -5,7 +5,7 @@ fn f_128(c: &mut Criterion) {
     let mut rng = thread_rng();
 
     let mut group = c.benchmark_group("f_128");
-    group.throughput(Throughput::Bytes(32u64)); // size of f-128 input block, in bytes
+    group.throughput(Throughput::Bytes(multimixer_128::BLOCK_SIZE as u64)); // size of f-128 input block, in bytes
 
     group.bench_function("f-128 (cached)", |bench| {
         let mut x = [0u32; 8];
